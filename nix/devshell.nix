a5c93690afef757f6d76efb7976c72ae9c6e07e4
Raphael Zimmermann <raphael.zimmermann@ergon.ch>
2025-02-26 07:58:09 +0100

    build: Prototype options search

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -5,7 +5,65 @@
   perSystem,
   ...
 }:
-perSystem.devshell.mkShell ({config, ...}: {
+perSystem.devshell.mkShell ({
+  config,
+  options,
+  lib,
+  ...
+}: let
+  # TODO: Create builder function that accepts pkgs, mappings list and options
+  sourceMappings = [
+    {
+      # TODO: magically derive via lockfile
+      flakeInput = {
+        inherit flake;
+        name = "devshell";
+      };
+
+      # But can be done manually as well
+      source = flake.inputs.devshell.outPath;
+      prefix = "https://github.com/numtide/devshell/blob/${flake.inputs.devshell.rev}";
+    }
+    # TODO: Test "self"
+    {
+      source = flake.outPath;
+      prefix = "";
+    }
+  ];
+
+  # TODO: Clean up
+  # TODO: Are there Hidden or Internal flags? eg. _module.args should be hidden
+  transformDeclarations = path: let
+    matches = lib.filter (mapping: lib.hasPrefix mapping.source (toString path)) sourceMappings;
+  in
+    if matches != []
+    then let
+      mapping = lib.head matches;
+      relativePath = lib.removePrefix mapping.source (toString path);
+    in
+      mapping.prefix + relativePath
+    else path;
+
+  optionsDoc = pkgs.nixosOptionsDoc {
+    inherit options;
+    transformOptions = opt:
+      opt
+      // {
+        declarations = builtins.map transformDeclarations opt.declarations;
+      };
+  };
+  showCommand = let
+    name = "show-options";
+    optionsJson = "${optionsDoc.optionsJSON}/share/doc/nixos/options.json";
+  in
+    pkgs.writeShellApplication {
+      inherit name;
+      runtimeInputs = [pkgs.jq pkgs.fzf pkgs.mdcat];
+      text = ''
+        jq -r 'keys[]' ${optionsJson} | fzf "$@" --preview 'jq -r --arg key {-1} -f ${./format_options.jq} ${optionsJson} | mdcat'
+      '';
+    };
+in {
   devshell = {
     name = ''Example Application'';
     startup.pre-commit.text = ''if [ -z "''${CI:-}" ]; then ${flake.checks.${system}.linters.shellHook} fi'';
@@ -42,6 +100,11 @@ perSystem.devshell.mkShell ({config, ...}: {
       name = "lint";
       help = "run all linters - or specific ones when passed as arguments";
       command = ''pre-commit run --all-files "''${@}"'';
+    }
+    {
+      name = "show-devshell-options";
+      help = "List and describe all available devshell options";
+      command = ''${lib.getExe showCommand} "''${@}"'';
     }
   ];
 })
